ee1f0adc8f023477d86b2e4b98531d40d1b87c32
ruitaiS <shaoruitai@gmail.com>
2020-10-23 09:09:27 -0400

    beginning to demystify tutorial.py

--- tutorial.py
+++ tutorial.py
@@ -35,10 +35,28 @@ for i in range(10):
 ##TODO: Read through code & understand it
 ## ----------------------------------------------------------------------
 
+#Determines activation output level of each node
+#Layers are represented as NDArrays
+#function(NDArray) will apply the function to every element in the array & return a new one
 def sigmoid(x):
     return 1 / (1 + np.e ** -x)
 activation_function = sigmoid
 
+
+'''
+Truncated Normal Distribution Function
+https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.truncnorm.html
+
+truncnorm(a,b,loc, scale)
+a,b >> the upper and lower bounds, calculated by:
+    a = (desired lower bound - mean) / standard deviation
+    b = (desired upper bound - mean) / standard deviation
+
+loc >> shifts the distribution:
+    Set it to the mean to center it
+
+scale >> scales the distribution
+'''
 def truncated_normal(mean=0, sd=1, low=0, upp=10):
     return truncnorm((low - mean) / sd, 
                      (upp - mean) / sd, 
@@ -61,20 +79,31 @@ class NeuralNetwork:
         
     def create_weight_matrices(self):
         """ 
-        A method to initialize the weight 
+        Initializes the weight 
         matrices of the neural network
+
+        X is a truncated normal distribution,
+        bounded between +/- 1/sqrt(# of input/hidden nodes)
+
+        X.rvs(M,N) generates a 2D array (M arrays of N length each)
+        from the normal distribution specified above
+        
+        Questions:
+            - Why choose the bounds for the weights as a function of the # of nodes?
+            - How exactly do the weights look in the structure of the NN?            
+        
         """
         rad = 1 / np.sqrt(self.no_of_in_nodes)
-        X = truncated_normal(mean=0, 
-                             sd=1, 
-                             low=-rad, 
-                             upp=rad)
-        self.wih = X.rvs((self.no_of_hidden_nodes, 
-                                       self.no_of_in_nodes))
+        X = truncated_normal(mean=0, sd=1, low=-rad, upp=rad)
+
+        #WIH == Weight of Inner, Hidden Nodes??
+        self.wih = X.rvs((self.no_of_hidden_nodes, self.no_of_in_nodes))
+        
         rad = 1 / np.sqrt(self.no_of_hidden_nodes)
         X = truncated_normal(mean=0, sd=1, low=-rad, upp=rad)
-        self.who = X.rvs((self.no_of_out_nodes, 
-                                         self.no_of_hidden_nodes))
+
+        #WHO == Weight of Hidden, Outer Nodes??
+        self.who = X.rvs((self.no_of_out_nodes, self.no_of_hidden_nodes))
         
     
     def train(self, input_vector, target_vector):
@@ -86,45 +115,36 @@ class NeuralNetwork:
         input_vector = np.array(input_vector, ndmin=2).T
         target_vector = np.array(target_vector, ndmin=2).T
         
-        output_vector1 = np.dot(self.wih, 
-                                input_vector)
+        output_vector1 = np.dot(self.wih, input_vector)
         output_hidden = activation_function(output_vector1)
         
-        output_vector2 = np.dot(self.who, 
-                                output_hidden)
+        output_vector2 = np.dot(self.who, output_hidden)
         output_network = activation_function(output_vector2)
         
         output_errors = target_vector - output_network
         # update the weights:
-        tmp = output_errors * output_network \
-              * (1.0 - output_network)     
-        tmp = self.learning_rate  * np.dot(tmp, 
-                                           output_hidden.T)
+        tmp = output_errors * output_network * (1.0 - output_network)     
+        tmp = self.learning_rate  * np.dot(tmp, output_hidden.T)
         self.who += tmp
 
 
         # calculate hidden errors:
-        hidden_errors = np.dot(self.who.T, 
-                               output_errors)
+        hidden_errors = np.dot(self.who.T, output_errors)
         # update the weights:
-        tmp = hidden_errors * output_hidden * \
-              (1.0 - output_hidden)
-        self.wih += self.learning_rate \
-                          * np.dot(tmp, input_vector.T)
-        
+        tmp = hidden_errors * output_hidden * (1.0 - output_hidden)
+        self.wih += self.learning_rate * np.dot(tmp, input_vector.T)
 
-        
-    
+
+
     def run(self, input_vector):
+
         # input_vector can be tuple, list or ndarray
         input_vector = np.array(input_vector, ndmin=2).T
 
-        output_vector = np.dot(self.wih, 
-                               input_vector)
+        output_vector = np.dot(self.wih, input_vector)
         output_vector = activation_function(output_vector)
         
-        output_vector = np.dot(self.who, 
-                               output_vector)
+        output_vector = np.dot(self.who, output_vector)
         output_vector = activation_function(output_vector)
     
         return output_vector
