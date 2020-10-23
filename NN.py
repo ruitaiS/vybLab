8dba520a7de3f35f05d27326927322b4cff44504
ruitaiS <shaoruitai@gmail.com>
2020-10-23 11:23:45 -0400

    pulled NN class from tutorial and put into NN.py; created v1 anomaly detector file

--- NN.py
+++ NN.py
@@ -1,39 +1,7 @@
-#Following this Tutorial:
-#https://www.python-course.eu/neural_network_mnist.php
-
 import numpy as np
 import matplotlib.pyplot as plt
 import pickle
 from scipy.stats import truncnorm
-
-
-with open("data/mnist/pickled_mnist.pkl", "br") as fh:
-    data = pickle.load(fh)
-
-train_imgs = data[0]
-test_imgs = data[1]
-train_labels = data[2]
-test_labels = data[3]
-train_labels_one_hot = data[4]
-test_labels_one_hot = data[5]
-
-image_size = 28 # width and length
-no_of_different_labels = 10 #  i.e. 0, 1, 2, 3, ..., 9
-image_pixels = image_size * image_size
-
-#Display First Ten 
-#(Close the window to see the next one)
-'''
-for i in range(10):
-    img = train_imgs[i].reshape((28,28))
-    plt.imshow(img, cmap="Greys")
-    plt.show()
-'''
-
-## Classifying ----------------------------------------------------------
-## RN this is just copy/paste
-##TODO: Read through code & understand it
-## ----------------------------------------------------------------------
 
 #Determines activation output level of each node
 #Layers are represented as NDArrays
@@ -181,29 +149,3 @@ class NeuralNetwork:
             else:
                 wrongs += 1
         return corrects, wrongs
-            
-
-ANN = NeuralNetwork(no_of_in_nodes = image_pixels, 
-                    no_of_out_nodes = 10, 
-                    no_of_hidden_nodes = 100,
-                    learning_rate = 0.1)
-    
-    
-for i in range(len(train_imgs)):
-    ANN.train(train_imgs[i], train_labels_one_hot[i])
-for i in range(20):
-    res = ANN.run(test_imgs[i])
-    print(test_labels[i], np.argmax(res), np.max(res))
-
-
-corrects, wrongs = ANN.evaluate(train_imgs, train_labels)
-print("accuracy train: ", corrects / ( corrects + wrongs))
-corrects, wrongs = ANN.evaluate(test_imgs, test_labels)
-print("accuracy: test", corrects / ( corrects + wrongs))
-
-cm = ANN.confusion_matrix(train_imgs, train_labels)
-print(cm)
-
-for i in range(10):
-    print("digit: ", i, "precision: ", ANN.precision(i, cm), "recall: ", ANN.recall(i, cm))
-
