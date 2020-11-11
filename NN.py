6e94667d435f68e4a649d761b52f5028ffa00ae6
ruitaiS <shaoruitai@gmail.com>
2020-11-11 13:54:15 -0500

    simplified NN

---
+++ NN.py
@@ -0,0 +1,104 @@
+#Based on this code:
+#https://www.python-course.eu/neural_network_mnist.php
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import truncnorm
+
+#Determines activation output level of each node
+#Layers are represented as NDArrays
+#function(NDArray) will apply the function to every element in the array & return a new one
+def sigmoid(x):
+    return 1 / (1 + np.e ** -x)
+activation_function = sigmoid
+
+def truncated_normal(mean=0, sd=1, low=0, upp=10):
+    return truncnorm((low - mean) / sd, 
+                     (upp - mean) / sd, 
+                     loc=mean, 
+                     scale=sd)
+
+class NeuralNetwork:
+    
+    def __init__(self, 
+                 no_of_in_nodes, 
+                 no_of_out_nodes, 
+                 no_of_hidden_nodes,
+                 learning_rate):
+        self.no_of_in_nodes = no_of_in_nodes
+        self.no_of_out_nodes = no_of_out_nodes
+        self.no_of_hidden_nodes = no_of_hidden_nodes
+        self.learning_rate = learning_rate 
+        self.create_weight_matrices()
+        
+    def create_weight_matrices(self):
+        rad = 1 / np.sqrt(self.no_of_in_nodes)
+        X = truncated_normal(mean=0, sd=1, low=-rad, upp=rad)
+
+        #WIH == Weight of Inner, Hidden Nodes(?)
+        self.wih = X.rvs((self.no_of_hidden_nodes, self.no_of_in_nodes))
+        
+        rad = 1 / np.sqrt(self.no_of_hidden_nodes)
+        X = truncated_normal(mean=0, sd=1, low=-rad, upp=rad)
+
+        #WHO == Weight of Hidden, Outer Nodes(?)
+        self.who = X.rvs((self.no_of_out_nodes, self.no_of_hidden_nodes))
+
+    def set_learning_rate(self, learning_rate):
+        self.learning_rate = learning_rate        
+        
+    
+    def train(self, input_vector, target_vector):
+        """
+        input_vector and target_vector can 
+        be tuple, list or ndarray
+        """
+        
+        input_vector = np.array(input_vector, ndmin=2).T
+        target_vector = np.array(target_vector, ndmin=2).T
+        
+        output_vector1 = np.dot(self.wih, input_vector)
+        output_hidden = activation_function(output_vector1)
+        
+        output_vector2 = np.dot(self.who, output_hidden)
+        output_network = activation_function(output_vector2)
+        
+        output_errors = target_vector - output_network
+        # update the weights:
+        tmp = output_errors * output_network * (1.0 - output_network)     
+        tmp = self.learning_rate  * np.dot(tmp, output_hidden.T)
+        self.who += tmp
+
+
+        # calculate hidden errors:
+        hidden_errors = np.dot(self.who.T, output_errors)
+        # update the weights:
+        tmp = hidden_errors * output_hidden * (1.0 - output_hidden)
+        self.wih += self.learning_rate * np.dot(tmp, input_vector.T)
+
+    def run(self, input_vector):
+
+        # input_vector can be tuple, list or ndarray
+        input_vector = np.array(input_vector, ndmin=2).T
+
+        output_vector = np.dot(self.wih, input_vector)
+        output_vector = activation_function(output_vector)
+        
+        output_vector = np.dot(self.who, output_vector)
+        output_vector = activation_function(output_vector)
+    
+        return output_vector
+
+    def equals(self, NN):
+        return ((np.equal(self.wih, NN.wih)) and (np.equal(self.who, NN.who)))
+
+
+'''
+create_weight_matrices generates a randomized weight matrix, and is called at initialization
+
+set_learning_rate allows you to modify the learning rate after the NN is already initialized
+    -> you also specify a learning rate when you first initialize the NN
+
+train and run operate on a single input (plus label, in the case of train)
+run is the only one that gives an output vector
+'''
---
+++ various snippets.py
@@ -0,0 +1,4 @@
+#Convert letter labels to alphanumeric characters
+def valueToChar(i):
+    letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    return letters[i]
