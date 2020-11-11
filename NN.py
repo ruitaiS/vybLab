6cb18a3367c1227fd9e4c2f289ebdf46922e62f8
Wissam Mantash <42943707+wissam2000@users.noreply.github.com>
2020-11-11 17:34:44 -0500

    NN is saved and same NN is used again next run

---
+++ NN.py
@@ -0,0 +1,179 @@
+#Based on this code:
+#https://www.python-course.eu/neural_network_mnist.php
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+from scipy.stats import truncnorm
+
+
+#Determines activation output level of each node
+#Layers are represented as NDArrays
+#function(NDArray) will apply the function to every element in the array & return a new one
+def sigmoid(x):
+    return 1 / (1 + np.e ** -x)
+activation_function = sigmoid
+
+
+'''
+Truncated Normal Distribution Function
+https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.truncnorm.html
+
+truncnorm(a,b,loc, scale)
+a,b >> the upper and lower bounds, calculated by:
+    a = (desired lower bound - mean) / standard deviation
+    b = (desired upper bound - mean) / standard deviation
+
+loc >> shifts the distribution:
+    Set it to the mean to center it
+
+scale >> scales the distribution
+'''
+def truncated_normal(mean=0, sd=1, low=0, upp=10):
+    return truncnorm((low - mean) / sd,
+                     (upp - mean) / sd,
+                     loc=mean,
+                     scale=sd)
+
+
+class NeuralNetwork:
+    output_hidden = 100
+    def __init__(self,
+                 no_of_in_nodes,
+                 no_of_out_nodes,
+                 no_of_hidden_nodes,
+                 learning_rate):
+        self.no_of_in_nodes = no_of_in_nodes
+        self.no_of_out_nodes = no_of_out_nodes
+        self.no_of_hidden_nodes = no_of_hidden_nodes
+        self.learning_rate = learning_rate
+        #self.create_weight_matrices()  #first we run v1 with  this to initialize the weights of the hidden layer, then we run load to preserve the network
+        self.load_weight_matrices()
+
+    def load_weight_matrices(self):
+        with open("wih_init.txt", "rb") as fp:   # Unpickling
+            self.wih = pickle.load(fp)
+        with open("who_init.txt", "rb") as fp:   # Unpickling
+            self.who = pickle.load(fp)
+    def create_weight_matrices(self):
+        """
+        Initializes the weight
+        matrices of the neural network
+
+        X is a truncated normal distribution,
+        bounded between +/- 1/sqrt(# of input/hidden nodes)
+
+        X.rvs(M,N) generates a 2D array (M arrays of N length each)
+        from the normal distribution specified above
+
+        Questions:
+            - Why choose the bounds for the weights as a function of the # of nodes?
+            - How exactly do the weights look in the structure of the NN?
+
+        """
+        rad = 1 / np.sqrt(self.no_of_in_nodes)
+        X = truncated_normal(mean=0, sd=1, low=-rad, upp=rad)
+
+        #WIH == Weight of Inner, Hidden Nodes??
+        self.wih = X.rvs((self.no_of_hidden_nodes, self.no_of_in_nodes))
+        with open("wih_init.txt", "wb") as fp:   #Pickling
+            pickle.dump(self.wih, fp)
+
+        rad = 1 / np.sqrt(self.no_of_hidden_nodes)
+        X = truncated_normal(mean=0, sd=1, low=-rad, upp=rad)
+
+        #WHO == Weight of Hidden, Outer Nodes??
+        self.who = X.rvs((self.no_of_out_nodes, self.no_of_hidden_nodes))
+        with open("who_init.txt", "wb") as fp:   #Pickling
+            pickle.dump(self.who, fp)
+
+    def train(self, input_vector, target_vector):
+        """
+        input_vector and target_vector can
+        be tuple, list or ndarray
+        """
+
+        input_vector = np.array(input_vector, ndmin=2).T
+        target_vector = np.array(target_vector, ndmin=2).T
+
+        output_vector1 = np.dot(self.wih, input_vector)
+
+        NeuralNetwork.output_hidden = activation_function(output_vector1)
+
+        output_vector2 = np.dot(self.who, NeuralNetwork.output_hidden)
+        output_network = activation_function(output_vector2)
+
+        output_errors = target_vector - output_network
+        # update the weights:
+        tmp = output_errors * output_network * (1.0 - output_network)
+        tmp = self.learning_rate  * np.dot(tmp, NeuralNetwork.output_hidden.T)
+        self.who += tmp
+
+
+        # calculate hidden errors:
+        hidden_errors = np.dot(self.who.T, output_errors)
+        # update the weights:
+        tmp = hidden_errors * NeuralNetwork.output_hidden * (1.0 - NeuralNetwork.output_hidden)
+        self.wih += self.learning_rate * np.dot(tmp, input_vector.T)
+
+
+
+    def run(self, input_vector):
+
+        # input_vector can be tuple, list or ndarray
+        input_vector = np.array(input_vector, ndmin=2).T
+
+        output_vector = np.dot(self.wih, input_vector)
+        output_vector = activation_function(output_vector)
+
+        output_vector = np.dot(self.who, output_vector)
+        output_vector = activation_function(output_vector)
+
+        return output_vector
+
+#---------------------------------------
+#Rest of this stuff is for presentation:
+#---------------------------------------
+
+    #TODO For mixed sets, show which letters / numbers get confused for numbers / letters
+    def confusion_matrix(self, data_array, labels):
+        cm = np.zeros((10, 10), int)
+        for i in range(len(data_array)):
+            res = self.run(data_array[i])
+            res_max = res.argmax()
+            target = labels[i][0]
+            cm[res_max, int(target)] += 1
+        return cm
+
+    def precision(self, label, confusion_matrix):
+        col = confusion_matrix[:, label]
+        return confusion_matrix[label, label] / col.sum()
+
+    def recall(self, label, confusion_matrix):
+        row = confusion_matrix[label, :]
+        return confusion_matrix[label, label] / row.sum()
+
+
+    #This can't be used for Meta, because the input data is actually pre-processed by another NN first
+    #Use metaEval instead
+    def evaluate(self, data, labels):
+        corrects, wrongs = 0, 0
+        for i in range(len(data)):
+            res = self.run(data[i])
+            res_max = res.argmax()
+            if res_max == labels[i]:
+                corrects += 1
+            else:
+                wrongs += 1
+        return corrects, wrongs
+
+    def metaEval(self, subNN, data, labels):
+        corrects, wrongs = 0, 0
+        for i in range(len(data)):
+            res = self.run(np.sort(subNN.run(data[i]).T))
+            res_max = res.argmax()
+            if res_max == labels[i]:
+                corrects += 1
+            else:
+                wrongs += 1
+        return corrects, wrongs
---
+++ v1.py
@@ -0,0 +1,181 @@
+#Based on this code:
+#https://www.python-course.eu/neural_network_mnist.php
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+from scipy.stats import truncnorm
+
+import random
+
+from NN import NeuralNetwork
+
+#Initializing Datasets:
+#------------------------------------------------------
+image_size = 28 # width and length
+image_pixels = image_size * image_size
+
+with open("data/mnist/pickled_mnist.pkl", "br") as fh:
+    data = pickle.load(fh)
+
+
+digits_train_imgs = data[0]
+digits_test_imgs = data[1]
+digits_train_labels = data[2]
+digits_test_labels = data[3]
+digits_train_labels_one_hot = data[4]
+digits_test_labels_one_hot = data[5]
+
+with open("data/emnist/pickled_emnist.pkl", "br") as fh:
+    data = pickle.load(fh)
+
+letters_train_imgs = data[0]
+letters_test_imgs = data[1]
+letters_train_labels = data[2]
+letters_test_labels = data[3]
+letters_train_labels_one_hot = data[4]
+letters_test_labels_one_hot = data[5]
+
+#TODO Ensure this way of splitting is methodologically sound
+
+#Labels are 0, 1 of whether something is a digit or letter
+#Values are the actual letter/digit being represented
+
+#Take last 1/3 of letters + digits training, make mixed training set
+#40000 elements in each (letters, digits, mixed)
+mixed_train_imgs = np.concatenate((digits_train_imgs[40000:], letters_train_imgs[40000:]))
+mixed_train_labels = np.concatenate((np.full(20000, 0), np.full(20000, 1)))
+
+mixed_train_values = np.concatenate((digits_train_labels[40000:], letters_train_labels[40000:]))
+
+#Take half of letters/digits test sets to make mixed test set
+#10k each in originals; 10k in mixed set
+mixed_test_imgs = np.concatenate((digits_test_imgs[5000:], letters_test_imgs[5000:]))
+mixed_test_labels = np.concatenate((np.full(5000, 0), np.full(5000, 1)))
+mixed_test_values = np.concatenate((digits_test_labels[5000:], letters_test_labels[5000:]))
+
+#Shuffle mixed images & labels so index matching is preserved
+#TODO Confirm this actually does it properly
+shuffler = np.random.permutation(len(mixed_train_imgs))
+mixed_train_imgs = mixed_train_imgs[shuffler]
+mixed_train_labels = mixed_train_labels[shuffler]
+
+shuffler2 = np.random.permutation(len(mixed_test_imgs))
+mixed_test_imgs = mixed_test_imgs[shuffler2]
+mixed_test_labels = mixed_test_labels[shuffler2]
+
+#Remove last 20k from both img and label for letters/digits
+digits_train_imgs = digits_train_imgs[:40000]
+letters_train_imgs = letters_train_imgs[:40000]
+digits_train_labels = digits_train_labels[:40000]
+letters_train_labels = letters_train_labels[:40000]
+
+#Remove last 5k from test img/label sets for letters/digits
+digits_test_imgs = digits_test_imgs[:5000]
+letters_test_imgs = letters_test_imgs[:5000]
+digits_test_labels = digits_test_labels[:5000]
+letters_test_labels = letters_test_labels[:5000]
+
+print(len(mixed_test_labels))
+print(len(mixed_test_imgs))
+#------------------------------------------------------
+
+#clear out bottom half of image (set rest to 0.01)
+digits_train_imgs_u = digits_train_imgs.copy()
+digits_test_imgs_u = digits_test_imgs.copy()
+def splicer(switch):
+    if(switch == "u"):
+        for i in range(0, len(digits_train_imgs_u)):
+            for j in range(392, 784):
+                digits_train_imgs_u[i][j] = 0.01
+        for i in range(0, len(digits_test_imgs_u)):
+            for j in range(392, 784):
+                digits_test_imgs_u[i][j] = 0.01
+    if(switch == "l"):
+        for i in range(0, len(digits_train_imgs_u)):
+            for j in range(0, 392):
+                digits_train_imgs_u[i][j] = 0.01
+        for i in range(0, len(digits_test_imgs_u)):
+            for j in range(0, 392):
+                digits_test_imgs_u[i][j] = 0.01
+
+Digit_NN = NeuralNetwork(no_of_in_nodes = image_pixels,
+                    no_of_out_nodes = 10,
+                    no_of_hidden_nodes = 100,
+                    learning_rate = 0.1)
+
+def iterate():
+    Digit_NN = NeuralNetwork(no_of_in_nodes = image_pixels,
+                        no_of_out_nodes = 10,
+                        no_of_hidden_nodes = 100,
+                        learning_rate = 0.1)
+
+    Letter_NN = NeuralNetwork(no_of_in_nodes = image_pixels,
+                        no_of_out_nodes = 26,
+                        no_of_hidden_nodes = 100,
+                        learning_rate = 0.1)
+
+    Meta_NN = NeuralNetwork(Digit_NN.no_of_out_nodes, 2, 100, 0.1)
+
+    #Train Digit NN
+def train(NN):
+    for i in range(len(digits_train_imgs_u)):
+        Digit_NN.train(digits_train_imgs_u[i], digits_train_labels_one_hot[i]) #    Digit_NN.train(digits_train_imgs[i], digits_train_labels_one_hot[i])
+
+    #Display Statistics for Digits
+    corrects, wrongs = Digit_NN.evaluate(digits_train_imgs_u, digits_train_labels)
+    print("digit accuracy train: ", corrects / ( corrects + wrongs))
+    corrects, wrongs = Digit_NN.evaluate(digits_test_imgs_u, digits_test_labels)
+    print("digit accuracy test:", corrects / ( corrects + wrongs))
+
+    #Train MetaNN, save NN output vectors to be evaluated later
+    #for i in range(len(mixed_train_imgs)):
+
+    #    Meta_NN.train(np.sort(Digit_NN.run(mixed_train_imgs[i]).T), mixed_train_labels[i])
+
+    #Display Statistics for Meta
+    #corrects, wrongs = Meta_NN.metaEval(Digit_NN, mixed_train_imgs, mixed_train_labels)
+    #train_accuracy = corrects / ( corrects + wrongs)
+    #print("Train Accuracy: ", train_accuracy)
+    #corrects, wrongs = Meta_NN.metaEval(Digit_NN, mixed_test_imgs, mixed_test_labels)
+    #test_accuracy = corrects / ( corrects + wrongs)
+    #print("Test Accuracy: ", test_accuracy)
+
+    #return train_accuracy, test_accuracy
+
+#Iterate 100 Times and Collect data
+
+def test(iterations):
+#    train_sum = 0
+#    test_sum = 0
+
+    for i in range(iterations):
+        train(Digit_NN)
+#        train_sum += a
+#        test_sum += b
+#    print("Train Average: ", train_sum / iterations)
+#    print("Test Average: ", test_sum / iterations)
+        #print(NeuralNetwork.output_hidden)
+
+splicer("u")
+test(100)
+
+
+
+#Roughly 0.5 without sorting
+#0.7ish with sorting, sometimes dips into 0.4s and .3's, but usually .6+
+#What is the deal with this variation??
+
+'''
+for i in range(len(digits_train_imgs)):
+    ANN.train(digits_train_imgs[i], digits_train_labels_one_hot[i])
+for i in range(20):
+    res = ANN.run(digits_test_imgs[i])
+    print(digits_test_labels[i], np.argmax(res), np.max(res))
+
+cm = ANN.confusion_matrix(digits_train_imgs, digits_train_labels)
+print(cm)
+
+for i in range(10):
+    print("digit: ", i, "precision: ", ANN.precision(i, cm), "recall: ", ANN.recall(i, cm))
+'''
