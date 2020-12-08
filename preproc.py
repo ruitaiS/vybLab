f150e1cc0a6bd9c133f3a9295324d52c3c669bb5
ruitaiS <shaoruitai@gmail.com>
2020-12-07 21:19:44 -0500

    cleanup, removed one-hotted labels

--- preproc.py
+++ preproc.py
@@ -6,13 +6,6 @@ import matplotlib.pyplot as plt
 import pickle
 
 def process(data_path, train_file, test_file, output_path):
-    #Initialization / Loading Data into Arrays
-    image_size = 28 # width and length
-    no_of_different_labels = 10 #  i.e. 0, 1, 2, 3, ..., 9
-    image_pixels = image_size * image_size
-
-    #These files are mondo yuge so it takes forever
-    #Luckily we will pickle them so we only need to do this once
     train_data = np.loadtxt(data_path + train_file, 
                             delimiter=",")
     test_data = np.loadtxt(data_path + test_file, 
@@ -27,23 +20,12 @@ def process(data_path, train_file, test_file, output_path):
     train_labels = np.asfarray(train_data[:, :1])
     test_labels = np.asfarray(test_data[:, :1])
 
-    #Converting to one-hot representation
-    lr = np.arange(10)
-    train_labels_one_hot = (lr==train_labels).astype(np.float)
-    test_labels_one_hot = (lr==test_labels).astype(np.float)
-    train_labels_one_hot[train_labels_one_hot==0] = 0.01
-    train_labels_one_hot[train_labels_one_hot==1] = 0.99
-    test_labels_one_hot[test_labels_one_hot==0] = 0.01
-    test_labels_one_hot[test_labels_one_hot==1] = 0.99
-
     #Dump Everything to Pickle
     with open(output_path, "bw") as fh:
         data = (train_imgs, 
                 test_imgs, 
                 train_labels,
-                test_labels,
-                train_labels_one_hot,
-                test_labels_one_hot)
+                test_labels)
         pickle.dump(data, fh)
 
 process("data/mnist/", "mnist_train.csv", "mnist_test.csv", "data/mnist/pickled_mnist.pkl")
